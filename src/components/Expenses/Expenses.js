2c259d060098ab7644cdb2ceed77e6cd78ee6dd1
Buzato <joaobuzato@gmail.com>
2023-03-22 21:37:37 -0300

    adicionando filtro de ano e passando states

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,18 @@
 import Card from "../UI/Card"
 import ExpenseItem from "./ExpenseItem"
+import ExpensesFilter from "./ExpensesFilter"
 import "./Expenses.css"
+import {useState} from 'react';
+
 
 export default function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020')
+    const filterHandler = (year) => {
+        setFilteredYear(year) 
+    }
     return (
         <Card className="expenses">
+            <ExpensesFilter selectedYear={filteredYear} onSelect={filterHandler}/>
             {props.expenses.map(expense => <ExpenseItem expense={expense} />)}
         </Card>
     )
